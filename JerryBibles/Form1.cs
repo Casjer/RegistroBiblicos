4f54655a6002fd3a79425ff0b878b9ddeeed1e42
CHristelle <CHristelle@DESKTOP-MV2QIEM>
2018-09-06 14:20:11 -0400

    Add project files.

---
+++ JerryBibles/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JerryBibles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
